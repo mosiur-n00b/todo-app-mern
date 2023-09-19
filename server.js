e24c5fad7d6bcb73e5324b41fd2a4fa9dac3d68c
Mosiur Rahman <mosiur.n00b@gmail.com>
2023-09-19 20:59:12 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const app = express();
+
+// routes
+const todo = require("./routes/todo");
+
+// connect database
+connectDB();
+
+// initialize middleware
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) => res.send("Server up and running"));
+
+// use routes
+app.use("/api/todo", todo);
+
+// setting up port
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
